d09288424067769e8186cd59d75f07be770f9945
nelml <konpaku.youmi@gmail.com>
2020-07-21 21:00:17 +0900

    moudame

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,6 @@ class BooksController < ApplicationController
   	book = Book.new(book_params)
   	if book.save
   		redirect_to book_path(book.id), notice: 'Book was successfully created.'
-    else
-      @books = Book.all
-      render 'main'
     end
   end
   def update
